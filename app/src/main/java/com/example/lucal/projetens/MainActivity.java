afda2f5e1d90675603d25b627f4c58dfef217c29
Dreos <lucaloisel@gmail.com>
2018-05-15 18:08:26 +0200

    Ecriture des premiers test sur les methodes des sensors sans tests

--- app/src/main/java/com/example/lucal/projetens/MainActivity.java
+++ app/src/main/java/com/example/lucal/projetens/MainActivity.java
@@ -11,18 +11,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager mSensorManager;
+
     static private Sensor GravitySensor;
     static private Sensor LinearASensor;
     static private Sensor RotationSensor;
     static private Sensor GyroSensor;
-
+    private List<Sensor> liste ;
+    static private Log log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +35,19 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //recuperation du sensor manager
         mSensorManager  = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        // recuperation de tout les sensors actif sur la machine
+        liste = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        // test des sensor actifs
+        for(int i = 0 ; i <liste.size();i++){
+            Sensor tmp = liste.get(i);
+            System.out.println(tmp.toString());
+        }
+
+        // recuperation de certains sensors
         GravitySensor   = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         LinearASensor   = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         RotationSensor  = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
@@ -45,10 +61,8 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-        // The light sensor returns a single value.
-        // Many sensors return 3 values, one for each axis.
-        float lux = event.values[0];
-        // Do something with this sensor value.
+        //float lux = event.values[0];
+
     }
 
     @Override
