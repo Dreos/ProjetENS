08d3a0567a6e6836ede6b852ae4dee52f9a2ce6b
Dreos <lucaloisel@gmail.com>
2018-05-15 17:12:07 +0200

    creation des variables des sensors

--- app/src/main/java/com/example/lucal/projetens/MainActivity.java
+++ app/src/main/java/com/example/lucal/projetens/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.lucal.projetens;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,9 +14,25 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SensorManager mSensorManager;
+    static private Sensor GravitySensor;
+    static private Sensor LinearASensor;
+    static private Sensor RotationSensor;
+    static private Sensor GyroSensor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager  = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        GravitySensor   = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        LinearASensor   = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        RotationSensor  = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        GyroSensor      = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+
     }
 }
